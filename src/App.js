32552d6c0fe925b42ed3b9deca8caafd60187e89
ASSABBANE Mehdi <mhidoart@gmail.com>
2021-07-06 15:09:11 +0200

    adding validation schema which uses Yup Module instade of validate of formik

--- src/App.js
+++ src/App.js
@@ -4,29 +4,16 @@ import { useFormik } from "formik";
 import * as Yup from 'yup';
 
 function App() {
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Email non valide").required("le champ email est obligatoire"),
+    password: Yup.string().min(6, "password doit contenir au moins 6 charactères").required("le champ password est obligatoire")
+  });
   const formik = useFormik({
     initialValues: {
       email: "",
       password: ""
     },
-    validate: values => {
-      let errors = {};
-      if (!values.email) {
-        errors.email = " le champ email est obligatoire!";
-      } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(values.email)) {
-        errors.email = " email doit etre comme exemple@exemple.com";
-
-      }
-
-
-      if (!values.password) {
-        errors.password = " le champ password est obligatoire!";
-      } else if (values.password.length < 6) {
-        errors.password = " le champ password doit contenir au moins 6 charactères!";
-      }
-
-      return errors;
-    },
+    validationSchema,
     onSubmit: values => {
       console.log(values);
     }
@@ -40,12 +27,12 @@ function App() {
           <div className="card p-2">
             <form onSubmit={formik.handleSubmit}>
               <div className="mb-3">
-                <label for="email" className="form-label">Email address</label>
+                <label htmlFor="email" className="form-label">Email address</label>
                 <input onChange={formik.handleChange} onBlur={formik.handleBlur} name="email" type="email" className="form-control" id="email" aria-describedby="emailHelp" />
                 <div id="emailHelp" className="invalid-feedback d-block">{formik.touched.email && formik.errors.email}</div>
               </div>
               <div className="mb-3">
-                <label for="password" className="form-label">Password</label>
+                <label htmlFor="password" className="form-label">Password</label>
                 <input onChange={formik.handleChange} onBlur={formik.handleBlur} name="password" type="password" className="form-control" id="password" />
                 <div id="emailHelp" className="invalid-feedback d-block">{formik.touched.password && formik.errors.password}</div>
 
